0555bd74b4eabac9ba769cd7958e909a339b6846
NikifFleser <nik838241@googlemail.com>
2024-06-05 13:33:51 +0200

    betting disabled after kickoff

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, session, g
 from flask import render_template, redirect, url_for, jsonify
 from flask_wtf.csrf import CSRFProtect
-from db import init_db, get_db
+from db import init_db, get_db, update_bet_in_db
 from auth import auth_bp, requires_admin, requires_login
 from requests import get as get_from
 from api import initial_fill_db, get_current_matchday
+from datetime import datetime, timedelta
 
 
 app = Flask(__name__)
@@ -41,14 +42,19 @@ def bet(matchday):
     if username == "admin":
         return redirect(url_for("auth.logout"))
     db = get_db(DATABASE)
-    match_tuples = db.execute("SELECT * FROM matches WHERE matchday = ?",(matchday,)).fetchall()
-    matches = []
+    match_tuples = db.execute("SELECT id, team1, team2, date FROM matches WHERE matchday = ?",(matchday,)).fetchall()
+    o_m = []
+    c_m = []
+    current_date = datetime.now() + timedelta(days=11)
     for match in match_tuples:
         m_id = match[0]
+        m_date = datetime.strptime(match[3], '%Y-%m-%dT%H:%M:%S')
         bet = db.execute("SELECT team1_goals, team2_goals FROM bets WHERE user_id = ? and match_id = ?", (session["user_id"], m_id)).fetchone()
-        matches.append((m_id, match[1], match[2], bet[0], bet[1]))
-
-    return render_template("bet.html", matches=matches, username=username, matchday=matchday)
+        if m_date < current_date:
+            c_m.append((m_id, match[1], match[2], bet[0], bet[1]))
+        else:
+            o_m.append((m_id, match[1], match[2], bet[0], bet[1]))
+    return render_template("bet.html", open_matches=o_m, closed_matches =c_m, username=username, matchday=matchday)
 
 @app.route('/update_bet', methods=['POST'])
 def update_bet():
@@ -57,13 +63,9 @@ def update_bet():
     team = data.get('team')
     goals = data.get('goals')
     user_id = session["user_id"]
-    update_bet_in_db(match_id, team, goals, user_id)
-    return jsonify({'status': 'success', 'match_id': match_id, 'team': team, 'goals': goals})
-
-def update_bet_in_db(match_id, team, goals, user_id):
-    db = get_db(DATABASE)
-    db.execute(f"UPDATE bets SET {team}_goals = ? WHERE match_id = ? and user_id = ?", (goals, match_id, user_id))
-    db.commit()
+    if update_bet_in_db(DATABASE, match_id, team, goals, user_id):
+        return jsonify({'status': 'success', 'match_id': match_id, 'team': team, 'goals': goals})
+    return redirect(url_for("bet_route"))
 
 # Close the database connection at ?request? end
 @app.teardown_appcontext
--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 from flask import g
+from datetime import datetime, timedelta
 import sqlite3
 
 def init_db(app, db_file):
@@ -16,4 +17,13 @@ def get_db(db_file):
         db  = sqlite3.connect(db_file)
         g.database = db
     return db
-   
+
+def update_bet_in_db(db_file, match_id, team, goals, user_id):
+    current_date = datetime.now()# + timedelta(days=10)
+    db = get_db(db_file)
+    match_date = db.execute("SELECT date FROM matches WHERE id = ?", (match_id,)).fetchone()
+    if datetime.strptime(match_date[0], '%Y-%m-%dT%H:%M:%S') > current_date:
+        db.execute(f"UPDATE bets SET {team}_goals = ? WHERE match_id = ? and user_id = ?", (goals, match_id, user_id))
+        db.commit()
+        return True
+    return False
